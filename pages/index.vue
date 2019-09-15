ab0eb4e1601ebde1e8b56967bd0c6216cca2ecc6
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2019-09-15 17:01:43 +0900

    add article

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,42 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        ts-nuxt
-      </h1>
-      <h2 class="subtitle">
-        My epic Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="wrapper">
+    <h1 class="title">
+      {{title}}
+    </h1>
+    <AppArticle :article="article" />
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<script lang="ts">
+import Vue from 'vue'
+import { Context } from '@nuxt/vue-app'
+import AppArticle from '../components/AppArticle.vue'
 
-export default {
-  components: {
-    Logo
+interface Data {
+  title: string
+}
+interface AsyncData {
+  article: {
+    created_at: string
+    title: string
+    author: string
+    body: string
   }
 }
+export default Vue.extend ({
+  components: {
+    AppArticle
+  },
+  data(): Data {
+    return {
+      title: 'Nuxt x TypeScript'
+    }
+  },
+  async asyncData({ query: $axios }: Context): Promise<AsyncData> {
+    const { data } = await $axios.get<ArticleData>(
+      `/api/v1/article/${query.page || 0}`
+    )
+    return { article: data.articel }
+  }
+})
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
---
+++ types/article.d.ts
@@ -0,0 +1,8 @@
+interface ArticleData {
+  article: {
+    created_at: string
+    title: string
+    author: string
+    body: string
+  }
+}
